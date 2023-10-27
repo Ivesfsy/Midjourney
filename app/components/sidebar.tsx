1c27b4bc70784d362835dca20add7ff4909df222
Ives Feng <97079241+Ivesfsy@users.noreply.github.com>
2023-10-27 12:15:07 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT-Midjourney
+          蓝衫ChatGPT-Midjourney
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Your own AI assistant.
+          Your own AI assistant.<a href="https://www.bluelsqkj.com/midjourney#div1">访问码购买</a>
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href="https://www.bluelsqkj.com/midjourney#div1" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
